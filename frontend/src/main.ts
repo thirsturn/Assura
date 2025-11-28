22c90f792353edd5c0fa353f4bd94858cbc04a1d
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2025-11-28 17:34:11 +0530

    deleted all

--- frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app'; // <-- 1. Fix import
-
-bootstrapApplication(AppComponent, appConfig)      // <-- 2. Fix class name
-  .catch((err) => console.error(err));
